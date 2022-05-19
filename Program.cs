8f0067549cf329707f7721d42e39e27e18ca3b20
badshasha <shavendragoesoft@gmail.com>
2022-05-20 04:58:59 +0530

    injection manual

--- Program.cs
+++ Program.cs
@@ -1,14 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extension.DependencyInjection;
+// using Microsoft.Extension.DependencyInjection;
 
 namespace injection
 {
     public class MainProgram{
 
         public static void Main(string[] args){
-            Console.WriteLine("working");
+            
 
-            // install 
+            IDomain DomainClass = new Domain();
+            Iui uiclass = new UI(DomainClass);
+            uiclass.PrintInfo();
+            
+        }
+
+    }
+
+    public interface IDomain{
+        void GetData();
+    }
+
+    public class Domain : IDomain
+    {
+        public void GetData(){
+            System.Console.WriteLine("Domain class information");
+        }
+    }
+
+    public interface Iui{
+        void PrintInfo();
+    }
+
+    public class UI : Iui
+    {
+        private readonly IDomain _dom;
+        public UI(IDomain _dom)
+        {
+            this._dom=  _dom;
+        }
+        public void PrintInfo(){           
+            _dom.GetData();
         }
 
     }

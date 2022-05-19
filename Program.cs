be481a13d9f6d464fc89703b5e63c46051d00fef
badshasha <shavendragoesoft@gmail.com>
2022-05-20 05:19:59 +0530

    scope and transient

--- Program.cs
+++ Program.cs
@@ -3,13 +3,33 @@ using Microsoft.Extension.DependencyInjection;
 
 namespace injection
 {
-    public class MainProgram{
 
+    public class ScopeClass{
+        public ScopeClass()
+        {
+            System.Console.WriteLine("scope is working");
+        }
+    }
+
+    public class Transient{
+        public Transient(){
+            System.Console.WriteLine("trnasient is working");
+        }
+    }
+
+    public class MainProgram{
         public static void Main(string[] args){
              
-
-           
-            
+                var collection = new ServiceCollection();
+                collection.AddScope<ScopeClass>();
+                collection.AddTransient<Transient>();
+                              
+                var builder = collection.buildServiceProvider();
+                
+                Parallel.For(1,10 ,i => {
+                    System.Console.WriteLine($"scope id { builder.GetService<ScopeClass>().GetHashCode().ToString() }");
+                    System.Console.WriteLine($"scope id { builder.GetService<Transient>().GetHashCode().ToString() }");
+                });
         }
 
     }

f4646dac27b66c2b959c19fde99f62fdcbede3c7
badshasha <shavendragoesoft@gmail.com>
2022-05-20 05:12:19 +0530

    clean

--- Program.cs
+++ Program.cs
@@ -8,46 +8,11 @@ namespace injection
         public static void Main(string[] args){
              
 
-            var collection = new SericeCollection();
-            collection.AddScope<IDomain,Domain>();
-
-            var provider = collection.BuildServiceProvied();
-            IDomain doaminInfo = provider.GetService<IDomain>();
-
-            // IDomain DomainClass = new Domain();
-            Iui uiclass = new UI(doaminInfo);
-            uiclass.PrintInfo();
+           
             
         }
 
     }
 
-    public interface IDomain{
-        void GetData();
-    }
-
-    public class Domain : IDomain
-    {
-        public void GetData(){
-            System.Console.WriteLine("Domain class information");
-        }
-    }
-
-    public interface Iui{
-        void PrintInfo();
-    }
-
-    public class UI : Iui
-    {
-        private readonly IDomain _dom;
-        public UI(IDomain _dom)
-        {
-            this._dom=  _dom;
-        }
-        public void PrintInfo(){           
-            _dom.GetData();
-        }
-
-    }
     
 }
